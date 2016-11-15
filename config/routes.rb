6a9bf1fefc348a8685836c2cc9eadec455017ac2
Guilherme Pontes <guilhermepontes@msn.com>
2016-11-15 19:37:54 +0100

    Add credit page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 	root 'home#index'
 
 	get '/get_country', to: 'home#get_country'
+
 	get '/map', to: 'map#index'
-    get '/request-articles', to: 'file#index'
+    get '/requested-articles', to: 'file#index'
+    get '/credits', to: 'credit#index'
+    get '/downloads', to: 'download#index'
 end
